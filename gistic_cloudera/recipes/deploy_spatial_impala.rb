ec22c6f98ba258569ca6f55dce5428e4743c10ba
Mostafa Ali <mostafaali@Mostafas-MacBook-Pro.local>
2015-06-18 20:38:36 +0300

    Increase log level to debug

--- gistic_cloudera/recipes/deploy_spatial_impala.rb
+++ gistic_cloudera/recipes/deploy_spatial_impala.rb
@@ -23,39 +23,41 @@ Chef::Log.level = :debug
 classpath_str = ""
 
 bucket.objects.each do |file|
-	paths['s3-to-local-map'].each do |prefix, target_prefix|
-		if file.key.start_with? prefix
+	paths['s3-to-local-map'].each do |prefix, target_prefixes|
+		target_prefixes.each do |target_prefix|
+			if file.key.start_with? prefix
 
-			targetPath = file.key.gsub prefix, target_prefix		
+				targetPath = file.key.gsub prefix, target_prefix		
 
-			if file.key.end_with? "/"  # Directory
-				puts "Dir : " + targetPath				
-				
-				directory targetPath do
-					mode 0755
-					recursive true
-					owner 'cloudera-scm'
-					action :create
+				if file.key.end_with? "/"  # Directory
+					puts "Dir : " + targetPath				
+					
+					directory targetPath do
+						mode 0755
+						recursive true
+						owner 'cloudera-scm'
+						action :create
+					end
+				else
+					puts "Reading file contents : " + file.key												
+					
+					puts "Putting file '" + file.key + "' into ---> " + targetPath
+
+					if prefix =~ /.*deps.*/
+						classpath_str += targetPath + ":"
+					end
+
+					puts "Creating the file ... "
+
+					remote_file targetPath do
+						source file.url_for(:read, :expires => 300).to_s.gsub(/https:\/\/([\w\.\-]*)\.{1}s3.amazonaws.com:443/, 'https://s3.amazonaws.com:443/\1') # Fix for ssl cert issue
+					  	action :create
+					  	owner 'cloudera-scm'
+					  	group 'cloudera-scm'
+					  	mode '0755'
+					end
+
 				end
-			else
-				puts "Reading file contents : " + file.key												
-				
-				puts "Putting file '" + file.key + "' into ---> " + targetPath
-
-				if prefix =~ /.*deps.*/
-					classpath_str += targetPath + ":"
-				end
-
-				puts "Creating the file ... "
-
-				remote_file targetPath do
-					source file.url_for(:read, :expires => 300).to_s.gsub(/https:\/\/([\w\.\-]*)\.{1}s3.amazonaws.com:443/, 'https://s3.amazonaws.com:443/\1') # Fix for ssl cert issue
-				  	action :create
-				  	owner 'cloudera-scm'
-				  	group 'cloudera-scm'
-				  	mode '0755'
-				end
-
 			end
 		end
 	end
