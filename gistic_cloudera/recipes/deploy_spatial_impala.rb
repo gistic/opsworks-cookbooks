77b91efff40c429680c8f9026f8742bb3696b8d2
Mostafa Ali <mostafaali@Mostafas-MacBook-Pro.local>
2015-06-18 05:42:03 +0300

    Printing the resulting class path

--- gistic_cloudera/recipes/deploy_spatial_impala.rb
+++ gistic_cloudera/recipes/deploy_spatial_impala.rb
@@ -7,8 +7,10 @@ AWS_REGION="eu-central-1"
 
 #credentials = Aws::Credentials.new(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY);
 
-s3 = Aws::S3::Client.new(region:AWS_REGION, 
-    credentials: credentials)
+#s3 = Aws::S3::Client.new(region:AWS_REGION, 
+#    credentials: credentials)
+
+s3 = AWS::S3.new
 
 # Read paths
 
