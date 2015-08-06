d2c5fb5aa094d999addecf38eeaf74de729c286d
Mostafa Ali <mostafaali@Mostafas-MacBook-Pro.local>
2015-08-06 18:58:09 +0300

    Backup and Restore DFS data files

--- gistic_cloudera/recipes/cm_restore_dfs_dir.rb
+++ gistic_cloudera/recipes/cm_restore_dfs_dir.rb
@@ -2,5 +2,6 @@ require 'resolv'
 
 execute "backup the dfs dir" do
   command "cp -rf /home/ubuntu/dfs-backup /mnt/dfs"
+  command "rm -rf /home/ubuntu/dfs-backup"
   command "chown cloudera-scm:cloudera-scm /mnt/dfs"
 end
